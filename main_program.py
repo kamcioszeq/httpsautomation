fd43b805b3fda8a4ebfa2a123738ffbe48ad8b25
kamcioszeq <kamil9431@gmail.com>
2019-09-13 13:39:48 +0200

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -7,45 +7,47 @@ from datetime import datetime
 import os
 import sys
 
+#global variables
 filepath = "stats.csv"
 result = []
 sumator = []
-
+#provide argument to cmd
 filesites = sys.argv[-1]
 
+#static variables to stat folder and files
 path = os.path.join(os.path.expanduser('/'), 'var', 'log', 'stats', 'logs.csv')
 path_statistics = os.path.join(os.path.expanduser('/'), 'var', 'log', 'stats', 'statistics.csv')
 
-
+#verify if http request get response
 def request_site(url):
     status_code = urllib.request.urlopen(f"http://{url}").getcode()
     if status_code == 200:
-        print(url + "Status Code 200")
+        print(url + "Status Code 200") #http request passed
         return status_code
     else:
-        print(url + "Url is unreachable")
+        print(url + "Url is unreachable") #http request failes, website is unreachable
         return status_code
 
-
+#verify SSL port - 443
 def isSSL_open(url, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((url, port))
-        print("Connection open")
+        print("Connection open") #https port open
         return True
     except Exception:
-        print("Port 443 is closed")
+        print("Port 443 is closed") #https port closed
         return False
 
-
+#check ssl certificate
 def check_certificate(url):
     try:
         requests.get(f"https://{url}")
-        return ("SSL Certificate Valid")
+        return ("SSL Certificate Valid") #ssl certificate is valid - connection is secure
     except Exception:
-        return ('SSL Certificate invalid')
+        return ('SSL Certificate invalid') #ssl certificate is invalid - connection is unsecure
 
-
+#generate logs for http and https request with ssl certificate validation
 def generate_csv_stats(url):
     status_code = request_site(url)
     if status_code == 200:
@@ -58,7 +60,7 @@ def generate_csv_stats(url):
     return {"url:": url, "status_code": status_code, "SSL open:": openSSL, "SSL Certificate:": cert,
             "Date_time: ": datetime.now()}
 
-
+#save logs to /var/log/stats directory
 def write_to_file(filepath):
     with open(filepath, "a") as csvfile:
         records = [generate_csv_stats(url) for url in lines]
@@ -66,7 +68,7 @@ def write_to_file(filepath):
         writer = csv.DictWriter(csvfile, fieldnames=records[0].keys())
         writer.writerows(records)
 
-
+#create directory /var/log/stats with access rights
 def create_dir(stats_dir):
     access_rights = 0o777
     try:
@@ -75,7 +77,7 @@ def create_dir(stats_dir):
     except:
         print("Directory exist")
 
-
+#read log file and create statistics file statistics.csv
 def read_csv_log(path_logs, filepath):
     with open(path_logs) as file:
         csv_reader_object = list(csv.reader(file))
@@ -93,7 +95,7 @@ def read_csv_log(path_logs, filepath):
             count_percent = (counter * 100 / total)
             result.append(count_percent)
             sumator.append(total)
-
+    #save to csv statistics
     with open(filepath, 'w') as myfile:
         wr = csv.writer(myfile)
         for i in zip(lines, result):
@@ -101,10 +103,12 @@ def read_csv_log(path_logs, filepath):
 
 
 if __name__ == '__main__':
+    #open sites.txt file
     with open(filesites, "r") as f:
         lines = [line.rstrip('\n').replace(',','') for line in f]
         print(lines)
-
+    
+    #run functions
     create_dir("/var/log/stats")
     write_to_file(path)
-    getline = read_csv_log("/var/log/stats/logs.csv", path_statistics)
+    read_csv_log("/var/log/stats/logs.csv", path_statistics)
